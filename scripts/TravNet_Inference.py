daab530a680b5c2776aa70fdfedf5a7b87bd5b18
gianvstheworld <gianlucacapezzuto@usp.br>
2023-06-23 12:14:51 -0300

    Adding scripts to test different ablative studies and another one to check the inference time of the network

---
+++ scripts/TravNet_Inference.py
@@ -0,0 +1,67 @@
+# Script para calcular o tempo de inferência da rede para uma imagem qualquer
+import torch
+import time
+
+from PIL import Image
+from torchvision import transforms
+
+import sys
+
+sys.path.append('../src')
+
+from models.TravNet import TravNet
+from utils.Dataloader_Inference import TravNetDataset
+
+class Object(object):
+    pass
+
+params = Object() # Cria um objeto para armazenar os parâmetros
+# Parametros do modelo 
+params.pretrained = True
+params.load_network_path = None 
+params.input_size       = (424, 240)
+params.output_size      = (424, 240)
+params.output_channels  = 1
+params.bottleneck_dim   = 256
+
+# Carregando o modelo e passando os parâmetros 
+model = TravNet(params=params)
+state_dict = torch.load('/home/gian/Documentos/Códigos/IC/ablation-study/scripts/checkpoints/prechanges_25/best_predictor_depth.pth')
+new_state_dict = {}
+for k, v in state_dict.items():
+    name = k.replace("module.", "")  # Remove o prefixo "module."
+    new_state_dict[name] = v
+model.load_state_dict(new_state_dict)
+model.eval()
+
+# Carregando a imagem
+rgb = Image.open('../../data/rgb/rgb_ts_2021_11_09_16h15m31s_000000.tif')
+depth = Image.open('../../data/depth/depth_ts_2021_11_09_16h15m31s_000000.tif')
+
+# Transformações
+transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+dataset = TravNetDataset(rgb, depth, transform=transform)
+
+# Carregando o dataset
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
+
+# Obtendo os dados
+rgb, depth = next(iter(train_loader))
+
+# Envie a imagem para a GPU, se disponível
+if torch.cuda.is_available():
+    model = model.cuda()
+
+# Calculando o tempo de inferência
+start = time.time()
+with torch.no_grad():
+    output = model(rgb.cuda().float(), depth.cuda().float())
+end = time.time()
+
+print('Tempo de inferência: {:.4f} segundos'.format(end - start))
---
+++ src/models/TravNet_Filters.py
@@ -0,0 +1,163 @@
+# Descrição: Modelo TravNet (ResNet18 Unet) 
+# Importação de modelos tradicionais de redes neurais
+from torchvision import models
+
+# Importação de bibliotecas
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class TravNet_Filters(nn.Module):
+    '''
+    Classe para o modelo da rede neural TravNet (Modelo de rede neural usando a
+    arquitetura ResNet18 como base e adaptando-o para o formato Unet)
+    '''
+    def __init__(self, params) -> None:
+        '''
+        Construtor da classe
+        :param params: Parâmetros de configuração do modelo
+        '''
+        super().__init__() # Construtor da classe pai
+        model = models.resnet18(pretrained=params.pretrained) # Carrega o modelo ResNet18 pré-treinado
+
+        self.out_dim = (params.output_size[1], params.output_size[0]) # Dimensão da saída
+
+        # RGB encoder - carrega as camadas do modelo ResNet18
+        self.block1 = nn.Sequential(*(list(model.children())[:3])) # Cria um novo objeto a partir das três primeiras camadas da rede neural (Conv - BatchNorm - ReLu) 
+        self.block2 = nn.Sequential(model.maxpool, model.layer2) # MaxPool e três camadas de Conv - BatchNorm - ReLu
+        self.block3 = model.layer3 # Quatro camadas (Conv - BatchNorm - ReLu)
+        self.block4 = model.layer4 # Quatro camadas (Conv - BatchNorm - ReLu)
+        self.block5 = model.layer4 # Quatro camadas (Conv - BatchNorm - ReLu)
+        # Esses blocos serão usados para codificar a imagem de entrada em um vetor de características
+
+        # Depth Encoder - Alteração no número de canais de entrada em comparação ao código original
+        self.block1_depth = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, padding=3, stride=2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True))
+        self.block2_depth = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, stride=2, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True))
+        self.block3_depth = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=2, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True))
+        self.block4_depth = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=2, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True))
+        self.block5_depth = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=2, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True))
+
+        # Bottleneck - reduz o número de canais de saída do encoder antes da etapa de codidicação (evita overfitting)
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=params.bottleneck_dim, kernel_size=3, padding=1, bias=False),
+            # nn.BatchNorm2d(params.bottleneck_dim),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=params.bottleneck_dim, out_channels=256, kernel_size=3, padding=1, bias=False),
+            # nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True))
+
+        # Decoder - usado para decodificar o vetor de características gerado pelo encoder
+        self.convTrans1 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=512+256, out_channels=256, kernel_size=2, stride=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True))
+
+        self.convTrans2 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=512+128, out_channels=128, kernel_size=2, stride=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True))
+
+        self.convTrans3 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=256+64, out_channels=64, kernel_size=2, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True))
+
+        self.convTrans4 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=128+32, out_channels=32, kernel_size=2, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True))
+
+        self.convTrans5 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=64+32, out_channels=32, kernel_size=2, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=params.output_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(params.output_channels),
+            nn.Sigmoid())
+
+    def forward(self, rgb_img, depth_img) -> torch.Tensor:
+        '''
+        Método forward da rede neural, processa as imagens de entrada e retorna a imagem de saída
+        :param rgb_img: Imagem RGB de entrada
+        :param depth_img: Imagem de profundidade de entrada
+        '''
+
+        # Encoder - capturando contexto da imagem
+        out1 = self.block1(rgb_img)
+        out1_depth = self.block1_depth(depth_img)
+        out1 = out1 + out1_depth
+        out2 = self.block2(out1)
+        out2_depth = self.block2_depth(out1_depth)
+        out2 = out2 + out2_depth
+        out3 = self.block3(out2)
+        out3_depth = self.block3_depth(out2_depth)
+        out3 = out3 + out3_depth
+        out4 = self.block4(out3)
+        out4_depth = self.block4_depth(out3_depth)
+        out4 = out4 + out4_depth
+        out5 = self.block5(out4)
+        out5_depth = self.block5_depth(out4_depth)
+        out5 = out5 + out5_depth
+
+        # Bottleneck - reduz o número de canais de saída do encoder antes da etapa de codidicação 
+        x = self.bottleneck(out5)
+
+        # Decoder
+        x = torch.cat((x, out5), dim=1)
+        x = self.convTrans1(x)
+        diffY = out4.size()[2] - x.size()[2]
+        diffX = out4.size()[3] - x.size()[3]
+        x = F.pad(x, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        x = torch.cat((x, out4), dim=1)
+        x = self.convTrans2(x)
+        diffY = out3.size()[2] - x.size()[2]
+        diffX = out3.size()[3] - x.size()[3]
+        x = F.pad(x, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+        x = torch.cat((x, out3), dim=1)
+        x = self.convTrans3(x)
+        x = torch.cat((x, out2), dim=1)
+        x = self.convTrans4(x)
+        x = torch.cat((x, out1), dim=1)
+        x = self.convTrans5(x)
+        
+        x = F.interpolate(x, size=self.out_dim)
+        return x
